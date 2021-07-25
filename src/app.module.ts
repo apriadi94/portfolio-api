e3dd7aeae8d39442f549f0ae550b0edb14c314c6
Apriadi <dev.apriadi@gmail.com>
2021-07-25 19:45:48 +0700

    add configuration for secret global variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/portfolio')],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DATABASE_SERVER)],
   controllers: [],
   providers: [],
 })
